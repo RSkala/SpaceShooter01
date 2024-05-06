cd5ea9d67d8fff4da53c450ab48311a7ba0dea97
RSkala <RichardSkala@gmail.com>
2024-05-05 18:19:43 -0700

    Face ship direction towards movement input if there is no look direction input

--- SpaceShooter01-Proj/Assets/Scripts/PlayerController.cs
+++ SpaceShooter01-Proj/Assets/Scripts/PlayerController.cs
@@ -32,23 +32,30 @@ public class PlayerController : MonoBehaviour
         // Update Movement
         if(!_moveDirectionInput.Equals(Vector2.zero))
         {
-
             // Movement amount
             Vector2 moveAmount = _rigidbody2D.position + _moveDirectionInput * _moveSpeed * Time.fixedDeltaTime;
             _rigidbody2D.MovePosition(moveAmount);
-        }
-        else
-        {
-            // No movement. Hide the 
-        }
 
-        if(!_lookDirectionInput.Equals(Vector2.zero))
-        {
-            Vector3 cross = Vector3.Cross(Vector2.up, _lookDirectionInput);
-            float flipValue = cross.z < 0.0f ? -1.0f : 1.0f;
-            float rotateAngle = Vector2.Angle(Vector2.up, _lookDirectionInput) * flipValue;
+            // Update the Look Direction. Note that this is expected to be overwritten later if the player has any look direction input.
+            float rotateAngle = CalculateRotationAngleFromInputDirection(_moveDirectionInput);
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotateAngle);
         }
+
+        // Update Look Direction
+        if(!_lookDirectionInput.Equals(Vector2.zero))
+        {
+            float rotateAngle = CalculateRotationAngleFromInputDirection(_lookDirectionInput);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotateAngle);
+        }
+    }
+
+    float CalculateRotationAngleFromInputDirection(Vector2 inputDirection)
+    {
+        Vector3 cross = Vector3.Cross(Vector2.up, inputDirection);
+        float flipValue = cross.z < 0.0f ? -1.0f : 1.0f;
+        float rotateAngle = Vector2.Angle(Vector2.up, inputDirection) * flipValue;
+        //float rotateAngle = Vector2.SignedAngle(Vector2.up, inputDirection);
+        return rotateAngle;
     }
 
     void UpdateShipExhaustVisual()
